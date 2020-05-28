ca6b8163f808b8fc9d2bab9e6b6cdf1c406ea382
Brian Meek <brian@slashtalk.com>
2020-05-27 19:26:30 -0700

    Fix active-win OSX helper to not crash on unexpected data; Check in binary

--- Sources/active-win/utilities.swift
+++ Sources/active-win/utilities.swift
@@ -1,17 +1,14 @@
 import AppKit
 
-
 @discardableResult
 func runAppleScript(source: String) -> String? {
 	NSAppleScript(source: source)?.executeAndReturnError(nil).stringValue
 }
 
-
 func toJson<T>(_ data: T) throws -> String {
 	let json = try JSONSerialization.data(withJSONObject: data)
 	return String(data: json, encoding: .utf8)!
 }
-
 
 // Show the system prompt if there's no permission.
 func hasScreenRecordingPermission() -> Bool {
